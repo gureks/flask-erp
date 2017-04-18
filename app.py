c9834a7b5bc99399c30c7c66c251b6694684555f
gureks <gurek15033@iiitd.ac.in>
2017-04-18 17:39:56 +0530

    fixed change username

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def logout():
 	session.pop('type') 
 	return redirect('/')
 
-@app.route('/dashboard')
-def homepage():
+@app.route('/settings')
+def settings():
 	if 'username' not in session:
 		return redirect('/')
 	cursor = mysql.connect().cursor()
@@ -49,12 +49,54 @@ def homepage():
 	data = cursor.fetchone()
 	if data is None:
 		return abort(404)
-	print(data)
+	return render_template('settings.html', data=data)
+
+@app.route('/dashboard')
+def dashboard():
+	if 'username' not in session:
+		return redirect('/')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT name, dob, sex, email, number, address FROM user, profile where user.username = \"" + session['username'] + "\" and user.username = profile.username")
+	data = cursor.fetchone()
+	if data is None:
+		return abort(404)
 	return render_template("dashboard.html",data=data)
 
 @app.route('/help')
 def help():
 	return render_template("help.html")
+
+@app.route('/changesettings', methods=['GET','POST'])
+def changesettings():
+	if 'username' not in session:
+		return redirect('/')
+	username = request.form.get('username')
+	password = request.form.get('password')
+	newAdmin = request.form.get('newAdmin')
+	msg = username + " " + password + " " + newAdmin + " " 
+	if username is not None:
+		if username == session['username']:
+			msg = msg + "<br />same username "
+		else:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute("SELECT username FROM user WHERE username = \"" + username + "\"")
+			data = cursor.fetchone()
+			if data is None:
+				msg = msg + "<br /> Username available"
+				print("UPDATE user SET username = \"" + username + "\" WHERE username = \"" + session['username'] + "\"" )
+				cursor.execute("UPDATE user SET username = \"" + username + "\" WHERE username = \"" + session['username'] + "\"" )
+				cursor.execute("UPDATE profile SET username = \"" + username + "\" WHERE username = \"" + session['username'] + "\"" )
+				data = cursor.fetchone()
+				print(data)
+				conn.commit()
+				session['username'] = username
+			else:
+				msg = msg + "<br />username already exists"
+	else:
+		msg = msg + "<br /> username is none"
+	return msg + "<br /> username now is " + session['username']	
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8000,debug=True)
